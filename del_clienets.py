983256d99a1416917bfb93a873f4c46b708c84d0
Maysa Arruda <maysatenorio502@gmail.com>
2024-02-16 11:25:47 -0300

    Adição do arquivo del_Clientes

---
+++ del_clienets.py
@@ -0,0 +1,23 @@
+import mysql.connector as mc 
+import os
+con = mc.connect(
+    host="127.0.0.1",
+    port="3784",
+    user="root",
+    password="senac@123",
+    database="banco"
+)
+os.system("cls")
+
+cursor = con.cursor()
+cursor.execute("Select * from Clientes")
+for c in cursor:
+    print(c)
+
+id = input("Digite o id do cliente que deseja apagar: ")
+rs = input("Você realmente deseja apagar este cliente? Digite (s ou n): ")
+if(rs=='s' or rs=='S'):
+    cursor.execute("delete from Clientes where clientes_id="+id)
+    con.commit()
+else:
+    print("------------------- Operação foi cancelada --------------------")
